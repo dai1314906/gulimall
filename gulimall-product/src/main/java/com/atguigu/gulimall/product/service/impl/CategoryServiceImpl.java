ffcbda5338843f675036169dee7782fa9f80cf7a
dailin <dailin359@gmail.com>
2026-05-30 20:38:45 +0800

    20260530-三级分类部分子节点菜单无法展示问题

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -112,7 +112,7 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
     private List<CategoryEntity> getChildrens(CategoryEntity root,List<CategoryEntity> all){
 
         List<CategoryEntity> children = all.stream().filter(categoryEntity -> {
-            return categoryEntity.getParentCid() == root.getCatId();
+            return categoryEntity.getParentCid().equals(root.getCatId());
         }).map(categoryEntity -> {
             //1、找到子菜单
             categoryEntity.setChildren(getChildrens(categoryEntity,all));
