b2c0bb1f1c8856199311e441654984fce988041c
dailin <dailin359@gmail.com>
2026-06-03 00:36:20 +0800

    20260603-删除接口配置获取的密钥注解属性字段

--- gulimall-third-party/src/main/java/com/atguigu/gulimall/thirdparty/controller/OssController.java
+++ gulimall-third-party/src/main/java/com/atguigu/gulimall/thirdparty/controller/OssController.java
@@ -27,7 +27,5 @@ public class OssController {
     @Value("${spring.cloud.alicloud.oss.bucket}")
     private String bucket;
 
-    @Value("${spring.cloud.alicloud.access-key}")
-    private String accessId;
 
 }
