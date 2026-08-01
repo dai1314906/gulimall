25e1675f81efa0d26f89b13c44370bd6f17da821
dailin <dailin359@gmail.com>
2026-08-02 00:42:13 +0800

    20260801：完成商城首页的界面开发

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
@@ -0,0 +1,81 @@
+package com.atguigu.gulimall.product.web;
+
+import com.atguigu.gulimall.product.entity.CategoryEntity;
+import com.atguigu.gulimall.product.service.CategoryService;
+import com.atguigu.gulimall.product.vo.Catelog2Vo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Controller
+public class IndexController {
+
+    @Autowired
+    CategoryService categoryService;
+
+
+
+
+    @GetMapping({"/","/index.html"})
+    public String indexPage(Model model){
+
+        System.out.println(""+Thread.currentThread().getId());
+        //TODO 1、查出所有的1级分类
+        List<CategoryEntity> categoryEntities =  categoryService.getLevel1Categorys();
+
+        // 视图解析器进行拼串：
+        // classpath:/templates/ +返回值+  .html
+        model.addAttribute("categorys",categoryEntities);
+        return "index";
+    }
+
+
+    //index/catalog.json
+    @ResponseBody
+    @GetMapping("/index/catalog.json")
+    public Map<String, List<Catelog2Vo>> getCatalogJson(){
+        Map<String, List<Catelog2Vo>> catalogJson = categoryService.getCatalogJson();
+        return catalogJson;
+    }
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello(){
+        //1、获取一把锁，只要锁的名字一样，就是同一把锁
+//        RLock lock = redisson.getLock("my-lock");
+
+        //2、加锁
+//        lock.lock(); //阻塞式等待。默认加的锁都是30s时间。
+        //1）、锁的自动续期，如果业务超长，运行期间自动给锁续上新的30s。不用担心业务时间长，锁自动过期被删掉
+        //2）、加锁的业务只要运行完成，就不会给当前锁续期，即使不手动解锁，锁默认在30s以后自动删除。
+
+//        lock.lock(10,TimeUnit.SECONDS); //10秒自动解锁,自动解锁时间一定要大于业务的执行时间。
+        //问题：lock.lock(10,TimeUnit.SECONDS); 在锁时间到了以后，不会自动续期。
+        //1、如果我们传递了锁的超时时间，就发送给redis执行脚本，进行占锁，默认超时就是我们指定的时间
+        //2、如果我们未指定锁的超时时间，就使用30 * 1000【LockWatchdogTimeout看门狗的默认时间】;
+        //    只要占锁成功，就会启动一个定时任务【重新给锁设置过期时间，新的过期时间就是看门狗的默认时间】,每隔10s都会自动再次续期，续成30s
+        //    internalLockLeaseTime【看门狗时间】 / 3,10s
+
+        //最佳实战
+        //1）、lock.lock(30,TimeUnit.SECONDS);省掉了整个续期操作。手动解锁
+        try{
+//            System.out.println("加锁成功，执行业务..."+Thread.currentThread().getId());
+//            Thread.sleep(30000);
+        }catch (Exception e){
+
+        }finally {
+            //3、解锁  将设解锁代码没有运行，redisson会不会出现死锁
+            System.out.println("释放锁..."+Thread.currentThread().getId());
+//            lock.unlock();
+        }
+
+        return "hello";
+    }
+}
